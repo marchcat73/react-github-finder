3dd4e99a75aaeee79731aa55883e8b12435a8052
Vitalij Vanyushin <vitalij.vanyushin@simbirsoft.com>
2019-08-01 21:40:34 +0400

    commit 3.6

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,18 +1,27 @@
-import React, { Component } from 'react'
+import React from 'react'
+import PropTypes from 'prop-types'
 
 import UserItem from './UserItem'
+import Spinner from '../layouts/Spinner'
 
-class Users extends Component {
+const Users = ({users, loading}) => {
+  if(loading) {
+    return <Spinner />
+  } else {
+    return (
+      <div style={userStyle}>
+        {users.map(user => (
+          <UserItem key={user.id} user={user} />
+        ))}
+      </div>
+    )
+  }
 
-	render() {
-		return (
-			<div style={userStyle}>
-				{this.props.users.map(user => (
-					<UserItem key={user.id} user={user} />
-				))}
-			</div>
-		)
-	}
+}
+
+Users.propTypes = {
+  users: PropTypes.array.isRequired,
+  loading: PropTypes.bool.isRequired
 }
 
 const userStyle = {
