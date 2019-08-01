5d9af9d5516e5aa478edf7ba2e777601ed1ab343
Vitalij Vanyushin <vitalij.vanyushin@simbirsoft.com>
2019-08-01 20:07:48 +0400

    commit 3.5

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
+import axios from 'axios'
 import Navbar from './components/layouts/Navbar'
 import Users from './components/users/Users'
 import './App.css';
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: false
+  }
+  async componentDidMount() {
+    this.setState({loading: true})
+    const res = await axios.get('https://api.github.com/users')
+    this.setState({users: res.data, loading: false})
+  }
 	render() {
-		// const numbers = [1,2,3,4]
 		return (
 			<div className='App'>
 				<Navbar />
-				<Users />
+        <div className="container">
+				  <Users loading={this.state.loading} users={this.state.users} />
+        </div>
 			</div>
 		);
 	}
--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,10 +1,7 @@
-import React, { Component } from 'react'
+import React from 'react'
+import PropTypes from 'prop-types'
 
-class UserItem extends Component {
-
-	render() {
-		const { login, avatar_url, html_url } = this.props.user
-		console.log(this.props.user)
+const UserItem = ({user: {login, avatar_url, html_url}}) => {
 		return (
 			<div className='card text-center'>
 				<img src={avatar_url} alt="" className='round-img' style={{ width: '60px' }} />
@@ -14,7 +11,10 @@ class UserItem extends Component {
 				</div>
 			</div>
 		)
-	}
+}
+
+UserItem.propTypes = {
+  user: PropTypes.object.isRequired,
 }
 
 export default UserItem
