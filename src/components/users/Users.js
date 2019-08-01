8a70a8c2231b9ea15986da8957b99e8897e51203
Vitaliy Vanyushin <vitalij.vanyushin@simbirsoft.com>
2019-08-01 17:14:53 +0400

    commit 3.3

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 
+import UserItem from './UserItem'
+
 class Users extends Component {
 	state = {
 		users: [
@@ -28,11 +30,19 @@ class Users extends Component {
 	}
 	render() {
 		return (
-			<div>
-				
+			<div style={userStyle}>
+				{this.state.users.map(user => (
+					<UserItem key={user.id} user={user} />
+				))}
 			</div>
 		)
 	}
+}
+
+const userStyle = {
+	display: 'grid',
+	gridTemplateColumns: 'repeat(3, 1fr)',
+	gridGap: '1rem'
 }
 
 export default Users
