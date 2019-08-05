5d3454975f30cc36979129b10d44cabc030b718e
Vitaliy Vanyushin <vitalij.vanyushin@simbirsoft.com>
2019-08-05 17:43:07 +0400

    commit 5.9

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import Navbar from './components/layouts/Navbar'
-import Users from './components/users/Users'
+import Home from './components/pages/Home'
 import User from './components/users/User'
-import Search from './components/users/Search'
 import Alert from './components/layouts/Alert'
 import About from './components/pages/About'
+import NotFound from './components/pages/NotFound'
 import GithubState from './context/github/GithubState'
 import AlertState from './context/alert/AlertState'
 
@@ -34,15 +34,11 @@ const App = () => {
 									<Route 
 										exact 
 										path='/' 
-										render={props => (
-										<Fragment>
-											<Search />
-											<Users />
-										</Fragment>
-										)} 
+										component={Home}
 									/>
 									<Route exact path='/about' component={About} />
 									<Route exact path='/user/:login' component={User} />
+									<Route component={NotFound} />
 								</Switch>
 							</div>
 						</div>
